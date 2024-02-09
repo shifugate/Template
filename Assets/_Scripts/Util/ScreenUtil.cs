17feb875fe00e230a5c7de5734281759c5ef93ea
shifugate <shifugate@gmail.com>
2024-02-09 07:46:24 +0000

    add

---
+++ Assets/_Scripts/Util/ScreenUtil.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+namespace Project.Util
+{
+    public static class ScreenUtil
+    {
+        public static bool PointerOverUIName()
+        {
+            GameObject go = PointerOverUIElement(GetEventSystemRaycastResults());
+
+            return go != null;
+        }
+
+        private static GameObject PointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+        {
+            if (eventSystemRaysastResults.Count > 0)
+                return eventSystemRaysastResults[0].gameObject;
+
+            return null;
+        }
+
+        private static List<RaycastResult> GetEventSystemRaycastResults()
+        {
+            PointerEventData eventData = new PointerEventData(EventSystem.current);
+            eventData.position = Input.mousePosition;
+            List<RaycastResult> raysastResults = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventData, raysastResults);
+            return raysastResults;
+        }
+    }
+}
